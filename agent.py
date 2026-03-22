f61b2f36db5665c444ca426ca320c5d834f226e0
ly232 <yang7.cooper@gmail.com>
2026-03-22 09:51:18 -0700

    Reinitialize flappybird env to prepare for native DQN implementation.

--- agent.py
+++ agent.py
@@ -1,192 +1,33 @@
-"""Module of all agents implementing different RL algorithms."""
+import flappy_bird_gymnasium
+import gymnasium
 
-from flappy_bird_gymnasium.envs.flappy_bird_env import Actions
-from jaxtyping import Float
-from stable_baselines3 import DQN
-from stable_baselines3.common.callbacks import CheckpointCallback
-from stable_baselines3.common.vec_env import DummyVecEnv, VecFrameStack
-from typing import override, Any
+env = gymnasium.make("FlappyBird-v0", render_mode="human", use_lidar=False)
+# env = gymnasium.make("CartPole-v1", render_mode="human")
 
-import abc
-import collections
-import itertools
+obs, _ = env.reset()
+while True:
+    # Next action:
+    # (feed the observation to your agent here)
+    action = env.action_space.sample()
 
-import matplotlib.pyplot as plt
-import numpy as np
+    # Processing:
+    # Example obs: [0.9861111111111112, 0.234375, 0.4296875, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0, 0.4609375, -0.8, 0.4666666666666667]
+    # the last pipe's horizontal position
+    # the last top pipe's vertical position
+    # the last bottom pipe's vertical position
+    # the next pipe's horizontal position
+    # the next top pipe's vertical position
+    # the next bottom pipe's vertical position
+    # the next next pipe's horizontal position
+    # the next next top pipe's vertical position
+    # the next next bottom pipe's vertical position
+    # player's vertical position
+    # player's vertical velocity
+    # player's rotation
+    obs, reward, terminated, _, info = env.step(action)
 
-import gymnasium as gym
-import flappy_bird_gymnasium  # Required import to register "FlappyBird-v0"
+    # Checking if the player is still alive
+    if terminated:
+        break
 
-
-# Observation assumes Lidar mode, which returns 180 distance readings from the
-# bird to the nearest obstacle in each degree direction.
-ObsType = Float[np.ndarray, "180"]
-ActType = Actions
-
-
-class Agent(abc.ABC):
-    """Abstract base class for all agents."""
-
-    def __init__(self):
-        self._total_rewards: list[float] = []
-
-    @abc.abstractmethod
-    def next_action(self, observation: ObsType) -> Actions:
-        """Selects an action based on the given observation.
-
-        Args:
-            observation: The current observation from the environment.
-
-        Returns:
-            The action selected by the agent.
-        """
-        pass
-
-    @abc.abstractmethod
-    def notify_reward_and_new_state(self, reward: float, new_observation: ObsType):
-        """Notifies the agent of a received reward and new observation.
-
-        The environment is responsible for always calling this method after
-        calling `step(action)` for an action produced by `next_action(observation)`.
-
-        Args:
-            reward: The reward received after taking an action.
-            new_observation: The new observation from the environment.
-        """
-        pass
-
-    @abc.abstractmethod
-    def notify_termination(self, trajectory: list[Any]):
-        """Notifies the agent of episode termination.
-
-        Args:
-            trajectory: The full trajectory of the episode.
-        """
-        pass
-
-    def plot_total_rewards(self) -> None:
-        """Plots the total rewards over episodes."""
-        plt.plot(self._total_rewards)
-        plt.xlabel("Episode")
-        plt.ylabel("Total Reward")
-        plt.title(f"Total Rewards over Episodes for {self.__class__.__name__}")
-        plt.show()
-
-
-class NaiveCyclicAgent(Agent):
-    """An naive agent that cycles through no-op and flap actions."""
-
-    def __init__(self, num_noops_till_flap: int = 15):
-        """Initializes the agent.
-
-        Args:
-            num_noops_till_flap: Number of no-op actions before a flap action.
-        """
-        super().__init__()
-        self._action_cycles = itertools.cycle(
-            [ActType.IDLE] * num_noops_till_flap + [ActType.FLAP]
-        )
-
-    @override
-    def next_action(self, observation: ObsType) -> ActType:
-        return next(self._action_cycles)
-
-    @override
-    def notify_reward_and_new_state(self, reward: float, new_observation: ObsType):
-        pass
-
-    @override
-    def notify_termination(self, trajectory: list[Any]):
-        rewards = trajectory[2::3]
-        print(f"total reward this episode: {sum(rewards)}")
-        self._total_rewards.append(sum(rewards))
-
-
-class MonteCarloTabularAgent(Agent):
-    """An agent that uses Monte Carlo every-visit tabular method to learn a policy."""
-
-    def __init__(self, gamma: float = 1.0):
-        super().__init__()
-        self._q: dict[tuple[ObsType, ActType], float] = collections.defaultdict(float)
-        self._n: dict[tuple[ObsType, ActType], int] = collections.Counter()
-        self._gamma = gamma
-        self._episode_count = 1
-
-    def _serialize_observation(self, obs: ObsType) -> str:
-        """Serializes the raw observations.
-
-        Note that to simplify state space, we only keep the middle 60 readings,
-        corresponding to +/- 30 degrees in front of the bird).
-        """
-        return tuple(np.round(obs[30:150:1], 2))
-
-    @override
-    def next_action(self, observation: ObsType) -> ActType:
-        observation = self._serialize_observation(observation)
-        # Greedy exploitation.
-        best_action, best_value = None, float("-inf")
-        for action in ActType:
-            q_value = self._q[(observation, action)]
-            if q_value > best_value:
-                best_value = q_value
-                best_action = action
-        # Epsilon-greedy exploration.
-        # eps = 1 / self._episode_count
-        eps = max(0.1, 1.0 / np.sqrt(self._episode_count + 1))
-        probabilities = {
-            action: (
-                eps / len(ActType) + (1 - eps)
-                if action == best_action
-                else (eps / len(ActType))
-            )
-            for action in ActType
-        }
-        actions = list(probabilities.keys())
-        weights = list(probabilities.values())
-        chosen_action = np.random.choice(actions, p=weights)
-        return chosen_action
-
-    @override
-    def notify_reward_and_new_state(self, reward: float, new_observation: ObsType):
-        # No-op because MC only updates at episode termination.
-        pass
-
-    @override
-    def notify_termination(self, trajectory: list[Any]):
-        self._episode_count += 1
-        # Construct visits counter.
-        gain = 0.0
-        states = trajectory[0::3]
-        actions = trajectory[1::3]
-        rewards = trajectory[2::3]
-        print(f"total reward this episode: {sum(rewards)}")
-        self._total_rewards.append(sum(rewards))
-        for s, a, r in reversed(list(zip(states, actions, rewards))):
-            gain = r + self._gamma * gain
-            key = (self._serialize_observation(s), a)
-            self._n[key] += 1
-            self._q[key] += 1 / self._n[key] * (gain - self._q[key])
-
-
-class DQNAgent(Agent):
-    """An agent that uses Deep Q-Network to learn a policy."""
-
-    def __init__(self, env: gym.Env):
-        super().__init__()
-        self._model = DQN.load("./model/dqn_flappy_bird", env=env)
-
-    @override
-    def next_action(self, observation: ObsType) -> ActType:
-        action, _states = self._model.predict(observation, deterministic=True)
-        return ActType(action)
-
-    @override
-    def notify_reward_and_new_state(self, reward: float, new_observation: ObsType):
-        # No-op because DQN handles learning internally.
-        pass
-
-    @override
-    def notify_termination(self, trajectory: list[Any]):
-        rewards = trajectory[2::3]
-        print(f"total reward this episode: {sum(rewards)}")
-        self._total_rewards.append(sum(rewards))
+env.close()
